aa9206bc95f0536853cf1169b7ebd0988928af89
Yanhao Li <hi@yanhaoli.com>
2020-12-17 16:03:48 -0500

    feat: canvas background color

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import { useEffect, useRef } from 'react';
 import './App.css';
 
 function App() {
+  const canvasEl = useRef();
+
+  useEffect(() => {
+    const canvas = canvasEl.current;
+    const gl = canvas.getContext("webgl2");
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Arm</h3>
+      <canvas ref={canvasEl} width="800" height="600">
+        Your browser does not support canvas
+      </canvas>
     </div>
   );
 }
