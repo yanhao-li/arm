c1d05e5c08b05ca0f4522e800e4f9ee2e3874c37
Yanhao Li <hi@yanhaoli.com>
2020-12-19 18:49:53 -0500

    feat: canvas pass control to arm

--- src/components/Arm.js
+++ src/components/Arm.js
@@ -1,10 +1,13 @@
+import { useEffect } from 'react';
 import Canvas from './Canvas'
 import { mat4, vec3 } from 'gl-matrix'
+import ArmVert from './shaders/arm.vert';
+import ArmFrag from './shaders/arm.frag';
 import { useCanvasContext } from '../CanvasContext';
 
 const Arm = () => {
 
-  const canvas = useCanvasContext();
+  // const { gl } = useCanvasContext();
 
   const camera = {
     position: vec3.set(vec3.create(), 6, 6, 4),
@@ -12,14 +15,60 @@ const Arm = () => {
     up_direction: vec3.set(vec3.create(), 0, 1, 0)
   };
 
-  const draw = () => {
+  const draw = (gl) => {
+
+    const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+    gl.shaderSource(vertexShader, ArmVert);
+    gl.shaderSource(fragmentShader, ArmFrag);
+    gl.compileShader(vertexShader);
+    gl.compileShader(fragmentShader);
+
+    const program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+      console.error('ERROR Linking program!', gl.getProgramInfoLog(program));
+      return;
+    }
+
+    gl.validateProgram(program);
+    if (!gl.getProgramParameter(program, gl.VALIDATE_STATUS)) {
+      console.error('ERROR validating program!', gl.getProgramInfoLog(program));
+      return;
+    }
+
+    const triangleVertices = [
+      0.0, 0.5,
+      -0.5, -0.5,
+      0.5, -0.5
+    ]
+
+    const buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices), gl.STATIC_DRAW);
     
+    const positionAttribLocation = gl.getAttribLocation(program, 'vertPosition');
+
+    gl.vertexAttribPointer(
+      positionAttribLocation,
+      2,
+      gl.FLOAT, 
+      gl.FALSE, 
+      2 * Float32Array.BYTES_PER_ELEMENT, 
+      0);
+
+    gl.enableVertexAttribArray(positionAttribLocation);
+    
+    gl.useProgram(program);
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
   }
 
-
-
   return (
-    <Canvas />
+    <Canvas onGlReady={draw}/>
   );
 }
 
--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,9 +1,7 @@
 import { useEffect, useRef } from 'react';
-import ArmVert from './shaders/arm.vert';
-import ArmFrag from './shaders/arm.frag';
 import { useCanvasUpdateContext } from '../CanvasContext';
 
-function Canvas() {
+const Canvas = ({onGlReady}) => {
   const canvasEl = useRef();
   const updateCanvas = useCanvasUpdateContext();
 
@@ -16,54 +14,7 @@ function Canvas() {
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    const vertexShader = gl.createShader(gl.VERTEX_SHADER);
-    const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-
-    gl.shaderSource(vertexShader, ArmVert);
-    gl.shaderSource(fragmentShader, ArmFrag);
-    gl.compileShader(vertexShader);
-    gl.compileShader(fragmentShader);
-
-    const program = gl.createProgram();
-    gl.attachShader(program, vertexShader);
-    gl.attachShader(program, fragmentShader);
-    gl.linkProgram(program);
-
-    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-      console.error('ERROR Linking program!', gl.getProgramInfoLog(program));
-      return;
-    }
-
-    gl.validateProgram(program);
-    if (!gl.getProgramParameter(program, gl.VALIDATE_STATUS)) {
-      console.error('ERROR validating program!', gl.getProgramInfoLog(program));
-      return;
-    }
-
-    const triangleVertices = [
-      0.0, 0.5,
-      -0.5, -0.5,
-      0.5, -0.5
-    ]
-
-    const buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices), gl.STATIC_DRAW);
-    
-    const positionAttribLocation = gl.getAttribLocation(program, 'vertPosition');
-
-    gl.vertexAttribPointer(
-      positionAttribLocation,
-      2,
-      gl.FLOAT, 
-      gl.FALSE, 
-      2 * Float32Array.BYTES_PER_ELEMENT, 
-      0);
-
-    gl.enableVertexAttribArray(positionAttribLocation);
-    
-    gl.useProgram(program);
-    gl.drawArrays(gl.TRIANGLES, 0, 3);
+    onGlReady(gl);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
