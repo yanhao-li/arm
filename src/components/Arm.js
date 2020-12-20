9dcd39867d264464a66e36ae28dc2dc54a26be94
Yanhao Li <hi@yanhaoli.com>
2020-12-19 20:26:43 -0500

    refactor: initBuffers

--- src/components/Arm.js
+++ src/components/Arm.js
@@ -1,8 +1,8 @@
-import { useEffect } from 'react';
 import Canvas from './Canvas'
-import { mat4, vec3 } from 'gl-matrix'
+import { vec3 } from 'gl-matrix'
 import vSource from './shaders/arm.vert';
 import fSource from './shaders/arm.frag';
+import { initBuffers } from '../Utils';
 
 const Arm = () => {
 
@@ -15,27 +15,23 @@ const Arm = () => {
 
   const draw = (gl) => {
 
-    const triangleVertices = [
+    const triangleVertices = new Float32Array([
       0.0, 0.5,
       -0.5, -0.5,
       0.5, -0.5
-    ]
+    ]);
 
-    const buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices), gl.STATIC_DRAW);
-    
-    const positionAttribLocation = gl.getAttribLocation(gl.program, 'vertPosition');
-
-    gl.vertexAttribPointer(
-      positionAttribLocation,
-      2,
-      gl.FLOAT, 
-      gl.FALSE, 
-      2 * Float32Array.BYTES_PER_ELEMENT, 
-      0);
-
-    gl.enableVertexAttribArray(positionAttribLocation);
+    initBuffers({
+      gl,
+      program: gl.program,
+      verticesData: [
+        {
+          attrVar: 'vertPosition',
+          vertices: triangleVertices,
+          size: 2,
+        }
+      ],
+    });
     
     gl.drawArrays(gl.TRIANGLES, 0, 3);
   }
