a9bd1b2763e5568346f9fa8a5339ad52b13618d9
Yanhao Li <hi@yanhaoli.com>
2020-12-21 15:46:05 -0500

    feat: added keyboard instruction

--- src/App.js
+++ src/App.js
@@ -6,6 +6,15 @@ function App() {
   return (
     <div className="App">
       <h3>WebGL Robot Arm</h3>
+      <ul>
+        <li><kbd>E</kbd>: Rotate the palm clockwisely</li>
+        <li><kbd>R</kbd>: Rotate the palm counter-clockwisely</li>
+        <li><kbd>↑</kbd>: Rise the arm</li>
+        <li><kbd>↓</kbd>: Fall the arm</li>
+        <li><kbd>←</kbd>: Rotate the base clockwisely</li>
+        <li><kbd>→</kbd>: Rotate the base counter-clockwisely</li>
+      </ul>
+      <br />
       <Arm />
     </div>
   );
